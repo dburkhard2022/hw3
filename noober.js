35dc38f8f4ae1dffd22618e2f474375ed4e9ef68
Drew Burkhard <drew.burkhard@kellogg.northwestern.edu>
2021-04-18 18:57:26 -0500

    hw completed

--- noober.js
+++ noober.js
@@ -13,5 +13,38 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  //declare drop off location variables and get them from the API
+  let dropoffAddress = ride.dropoffLocation.address
+  let dropoffCity = ride.dropoffLocation.city
+  let dropoffState = ride.dropoffLocation.state
+  let dropoffZip = ride.dropoffLocation.zip
+
+
+  //declare passenger details varialbes and get them from the API
+  let passengerFirst = ride.passengerDetails.first
+  let passengerLast = ride.passengerDetails.last
+  let passengerPhone = ride.passengerDetails.phoneNumber
+
+   //declare drop off location variables and get them from the API
+   let pickupAddress = ride.pickupLocation.address
+   let pickupCity = ride.pickupLocation.city
+   let pickupState = ride.pickupLocation.state
+   let pickupZip = ride.pickupLocation.zip
+
+   //declare variable of level of servie and declare it based on other details from API
+   let rideType
+   if (ride.purpleRequested == true) {
+     rideType = `Noober Purple`
+   } else if (ride.numberOfPassengers > 3) {
+     rideType = `Noober XL`
+   } else {
+     rideType = `Noober X`
+   }
+
+  //output message with ride details to console
+  console.log(`${rideType} Passenger: ${passengerFirst} ${passengerLast} - ${passengerPhone}. 
+  Pickup at ${pickupAddress}, ${pickupCity}, ${pickupState} ${pickupZip}. 
+  Drop off ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}.`)
+  
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
